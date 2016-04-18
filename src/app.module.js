8fbf773c36d01228a393364081e42d7f3a4e7665
Nicholas Johnson <nicholas@forwardadvance.com>
2016-04-18 22:43:37 +0100

    Exports app module

--- src/app.module.js
+++ src/app.module.js
@@ -1,8 +1,9 @@
 var angular = require('angular');
 var formly = require('angular-formly');
 var formlyBootstrap = require('angular-formly-templates-bootstrap');
-
 var formController = require('./form.controller');
 
-angular.module('app', [formly, formlyBootstrap])
-  .controller('formController', formController)
+var app = angular.module('app', [formly, formlyBootstrap])
+  .controller('formController', formController);
+
+module.exports = app.name;
