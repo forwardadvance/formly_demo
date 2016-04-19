d31ec78a4754cd30347cc865821418ddda9f60e6
Nicholas Johnson <nicholas@forwardadvance.com>
2016-04-19 10:28:11 +0100

    Breaks out userFields into a separate module

---
+++ src/form.user.fields.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    key: 'email',
+    type: 'input',
+    templateOptions: {
+      type: 'email',
+      label: 'Email address',
+      placeholder: 'Enter email'
+    }
+  },
+  {
+    key: 'password',
+    type: 'input',
+    templateOptions: {
+      type: 'password',
+      label: 'Password',
+      placeholder: 'Password'
+    }
+  },
+  {
+    key: 'checked',
+    type: 'checkbox',
+    templateOptions: {
+      label: 'Check me out'
+    }
+  }
+];
